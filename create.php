3318ed18eada0e2b5c2c178555fc9609f3ecd307
unknown <dani.perez.cespedes@gmail.com>
2023-01-10 13:31:24 +0100

    First commit

---
+++ create.php
@@ -0,0 +1,124 @@
+<?php
+//Define variables for Connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "dappi";
+
+//Create connection
+
+$connection = mysqli_connect($servername,$username,$password,$database);
+
+//define variables
+$first_name ="";
+$last_name ="";
+$email ="";
+$phone ="";
+$address="";
+$errorMessage="";
+$errorMessage2="";
+$successMessage="";
+
+//obtain the information filled by user in webpage
+
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+    $first_name =$_POST["first_name"];
+    $last_name = $_POST["last_name"];
+    $email = $_POST["email"];
+    $phone =$_POST["phone"];
+    $address=$_POST["address"];
+
+    do{
+        if (empty($first_name) || empty($last_name)||empty($email)||empty($phone)||empty($address))
+        {
+            $errorMessage ="Please fill all fields required";
+        break;
+        }
+        //add a new employee from the for, to the table "employees"
+
+        $sql="INSERT INTO employees (first_name,last_name,email,phone,address)
+                VALUES ('$first_name','$last_name','$email','$phone', '$address')";
+        $result = $connection -> query($sql);
+
+         //check if query is correct or not
+
+        if(!$result){
+           $errorMessage2 = "Invalid query: ". $connection ->error;
+           break; //break the while loop
+        }
+
+
+        $first_name ="";
+        $last_name ="";
+        $email ="";
+        $phone ="";
+        $address="";
+
+        $successMessage ="Employee added correctly";
+        //redirect user to main page
+       // header("location:/DAPPI/index.php");
+        //exit;
+
+    } while(false);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DAPPI</title>
+</head>
+<body>
+    <h2>New Employee</h2>
+    <?php
+    //check if error message is not empy. If not empty, show error message
+    if (!empty($errorMessage)){
+        echo "
+        <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+            <strong>$errorMessage</strong>
+        </div>
+        ";
+    }
+    ?>
+    <br>
+    <form method="post">
+        <!-add rows of information->
+        <label>First Name</label>
+        <input type ="text" class="form-control" name="first_name" value="<?php echo $first_name;?>">
+        <br>
+        <label>Last name</label>
+        <input type ="text" class="form-control" name="last_name" value="<?php echo $last_name;?>">  
+        <br>
+        <label>Email</label>
+        <input type ="text" class="form-control" name="email" value="<?php echo $email;?>">    
+        <br>
+        <label>Phone number</label>
+        <input type ="text" class="form-control" name="phone" value="<?php echo $phone;?>">      
+        <br>
+        <label>Address</label>
+        <input type ="text" class="form-control" name="address" value="<?php echo $address;?>">  
+        <br>
+        <br>  
+        
+        <!- check if success message is not empty, if is not, display success message->
+        <?php
+    if (!empty($successMessage)){
+        echo "
+        <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+            <strong>$successMessage</strong>
+            <a href='index.php'> Go to main page </a>
+        </div>
+        ";
+    }
+        ?>
+        <br>
+        <br>
+        <button type="submit" class = #btn btn-primary">Submit</button>
+        <a class="btn btn-outline-primary" href="/DAPPI/index.php" role="button">Cancel</a>
+    </form>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>List of Employees</h1>
+    <a class ="btn btn-primary" href="/DAPPI/create.php" role="button">Create a new employee</a>
+    <br>
+    <br>
+    <h2>List of Current Employees</h2>
+    <table class = "table">
+        <thead>
+            <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Address</th>
+            </tr>
+            <t/head>
+
+            <tbody>
+                <?php
+            //Define variables for connection
+
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $database = "dappi";
+
+            //Create connection
+
+            $connection = mysqli_connect($servername,$username,$password,$database);
+            
+            // Check connecion
+            if ($connection ->connect_error){
+                die("Connection failed: ". $connection ->connect_error);
+            }
+
+            //read all row from database table
+
+            $sql = "SELECT * FROM employees";
+            $result = $connection -> query($sql);
+
+            //check if query is correct or not
+
+            if(!$result){
+                die("Invalid query: ". $connection ->error);
+            }
+            
+            // read data of each row
+            while($row = mysqli_fetch_assoc($result)){
+                echo "<tr>
+                <td>" .$row["id"]. "</td>
+                <td>" .$row["first_name"]. "</td>
+                <td>" .$row["last_name"]. "</td>
+                <td>" .$row["email"]. "</td>
+                <td>" .$row["phone"]. "</td>
+                <td>" .$row["address"]. "</td>
+                </tr>";
+            }
+            ?>
+            </tbody>
+</table>
+</body>
+</html>
